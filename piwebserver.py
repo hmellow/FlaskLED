8cd83e291560070f574d8f231d3bd16345e6962c
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2021-11-24 17:19:38 -0600

    Removed incidental characters

--- piwebserver.py
+++ piwebserver.py
@@ -1,35 +1,27 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2578
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+from flask import Flask, render_template
+from smbus import SMBus
 
-\f0\fs24 \cf0 from flask import Flask, render_template\
-from smbus import SMBus\
+addr = 0x8
+bus = SMBus(1)
+
+app = Flask(__name__)
 \
-addr = 0x8\
-bus = SMBus(1)\
+@app.route('/')
+def index():
+	return render_template('ws.html')
 \
-app = Flask(__name__)\
-\
-@app.route('/')\
-def index():\
-	return render_template('ws.html')\
-\
-@app.route('/on-link/')\
-def on_link():\
- 	print ('On selected')\
- 	bus.write_byte(addr, 0x1)\
- 	return render_template("ws.html")\
-\
-@app.route('/off-link/')\
-def off_link():\
- 	print('Off selected')\
- 	bus.write_byte(addr, 0x0)\
- 	return render_template("ws.html")\
-\
-if __name__ == '__main__':\
- 	app.run(host='0.0.0.0')\
-\
+@app.route('/on-link/')
+def on_link():
+ 	print ('On selected')
+ 	bus.write_byte(addr, 0x1)
+ 	return render_template("ws.html")
+
+@app.route('/off-link/')
+def off_link():
+ 	print('Off selected')
+ 	bus.write_byte(addr, 0x0)
+ 	return render_template("ws.html")
+
+if __name__ == '__main__':
+ 	app.run(host='0.0.0.0')
 }
